dc501e303f8f56cea7addeff0d6d96763695b66b
ysk826 <ysuke826@gmail.com>
2024-05-24 08:34:48 +0900

    空文字のチェック処理追加

--- lib/register_button.dart
+++ lib/register_button.dart
@@ -18,17 +18,24 @@ class RegisterButton extends StatelessWidget {
     if (Platform.isIOS) {
       // 登録ボタン
       return CupertinoButton(
-          child: Text(label),
-          color: CupertinoColors.activeBlue,
-          // ボタンが押された時の処理
-          onPressed: () {
+        child: Text(label),
+        color: CupertinoColors.activeBlue,
+        // ボタンが押された時の処理
+        onPressed: () {
+          // テキストフィールドから入力された文字列を取得
+          String word = controller.text.trim();
+          // 入力された文字列が空でない場合
+          if (word.isNotEmpty) {
             showModalBottomSheet(
               // 画面全体を覆おうように設定
               isScrollControlled: true,
               context: context,
               builder: (BuildContext context) {
+                // 意味を入力するテキストフィールド
                 final meaningController = TextEditingController();
+                // モーダルの中身
                 return Container(
+                  // 画面全体(94%)を覆う高さに設定
                   height: MediaQuery.of(context).size.height * 0.94,
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
@@ -36,16 +43,21 @@ class RegisterButton extends StatelessWidget {
                     children: <Widget>[
                       const Text('Enter the meaning'),
                       TextField(controller: meaningController),
+                      // 保存ボタン
                       ElevatedButton(
                         child: const Text('Save'),
+                        // 保存ボタンが押された時の処理
                         onPressed: () {
-                          String word = controller.text;
-                          String meaning = meaningController.text;
+                          // 意味を取得
+                          String meaning = meaningController.text.trim();
+                          // 意味が空でない場合
+                          if (meaning.isNotEmpty) {
                           _databaseHelper.insertWord(word, meaning);
                           // text field clear
                           controller.clear();
                           // モーダルを閉じる
                           Navigator.pop(context);
+                          }
                         },
                       ),
                     ],
@@ -53,7 +65,9 @@ class RegisterButton extends StatelessWidget {
                 );
               },
             );
-          });
+          }
+        },
+      );
     } else {
       return ElevatedButton(
         child: Text(label),
@@ -91,4 +105,3 @@ class RegisterButton extends StatelessWidget {
     }
   }
 }
-
