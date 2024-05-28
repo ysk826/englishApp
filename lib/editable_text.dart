39fcf6c5d4f1ddc64527c4512b6072d8295da9ed
ysk826 <ysuke826@gmail.com>
2024-05-29 08:25:33 +0900

    編集画面をTextFromFieldで表示に変更

--- lib/editable_text.dart
+++ lib/editable_text.dart
@@ -17,8 +17,6 @@ class MyEditableText extends StatefulWidget {
 }
 
 class _MyEditableTextState extends State<MyEditableText> {
-  // 編集モードかどうかを判定するフラグ
-  bool _isEditing = false;
 
   @override
   void initState() {
@@ -28,45 +26,15 @@ class _MyEditableTextState extends State<MyEditableText> {
 
   @override
   Widget build(BuildContext context) {
-    // タッチイベントを検知するためのウィジェット
-    return GestureDetector(
-      onTap: () {
-        // タップされたら編集モードにする
-        setState(() {
-          _isEditing = true;
-        });
-      },
-      child: _isEditing
-          ? Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.1,
-              child: TextFormField(
-                controller: widget.controller,
-                // テキストフィールドのスタイルを設定
-                style: const TextStyle(
-                  fontSize: 18,
-                  height: 5,
-                ),
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.zero,
-                  // テキストフィールドの境界線を設定
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-            )
-          : Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.1,
-              child: Text(
-                widget.initialText,
-                style: const TextStyle(
-                  fontSize: 18,
-                  height: 5,
-                ),
-              ),
-            ),
+    return TextFormField(
+      controller: widget.controller,
+      style: const TextStyle(fontSize: 18),
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.zero,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
     );
   }
 }
