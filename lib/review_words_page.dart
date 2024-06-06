16c17a584dc9e9d07d46c7ebab26024112a82968
ysk826 <ysuke826@gmail.com>
2024-06-07 08:44:19 +0900

    再挑戦ボタン、終了ボタン追加

--- lib/review_words_page.dart
+++ lib/review_words_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'card_word.dart';
+import 'flashcard.dart';
 
 // 単語の復習ページ
 class ReviewWordsPage extends StatefulWidget {
@@ -16,16 +17,45 @@ class _ReviewWordsPageState extends State<ReviewWordsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Review Words'),
+        title: const Text('Review Words'),
       ),
-      body: ListView.builder(
-        itemCount: widget.allWordsCopy.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(widget.allWordsCopy[index].word),
-            subtitle: Text(widget.allWordsCopy[index].meaning),
-          );
-        },
+      // スクロール可能なウィジェット
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 500,
+              child: ListView.builder(
+                // ListViewの範囲はスクロールができる
+                shrinkWrap: true,
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemCount: widget.allWordsCopy.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(widget.allWordsCopy[index].word),
+                    subtitle: Text(widget.allWordsCopy[index].meaning),
+                  );
+                },
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('try again'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Flashcard()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('end'),
+              onPressed: () {
+                Navigator.popUntil(
+                    context, ModalRoute.withName(Navigator.defaultRouteName));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
